8b5d2ce6c6210f4c8cc2094c9417d22ae5697ee4
Terrel Shumway <moan-o-storm-beware-spoke@shumway.us>
2016-10-18 00:40:44 -0600

    implement enscrypt with test vectors

--- sqrl/crypto.py
+++ sqrl/crypto.py
@@ -2,6 +2,7 @@
 from sqrl import KEY_BYTES
 from pysodium import sodium
 import ctypes
+from time import process_time
 
 
 def enhash(data, iterations=16):
@@ -14,3 +15,35 @@ def enhash(data, iterations=16):
         sodium.crypto_hash_sha256(u, u, ld)
         acc ^= int.from_bytes(u, 'little')
     return acc.to_bytes(32, 'little')
+
+_kdf = sodium.crypto_pwhash_scryptsalsa208sha256_ll
+
+
+def enscrypt(passwd, salt, logN, iterations, seconds=0):
+    pwlen = ctypes.c_size_t(len(passwd))
+    saltlen = ctypes.c_size_t(len(salt))
+    N = ctypes.c_uint64(1 << logN)
+    r = ctypes.c_uint32(256)
+    p = ctypes.c_uint32(1)
+    outlen = ctypes.c_size_t(KEY_BYTES)
+    out = ctypes.create_string_buffer(KEY_BYTES).raw
+    _kdf(
+        passwd, pwlen,
+        salt, saltlen,
+        N, r, p,
+        out, outlen,
+    )
+    acc = int.from_bytes(out, 'little')
+    i = 1
+    start = process_time()
+    end = start + seconds
+    while i < iterations or process_time() < end:
+        _kdf(
+            passwd, pwlen,
+            out, outlen,
+            N, r, p,
+            out, outlen,
+        )
+        acc ^= int.from_bytes(out, 'little')
+        i += 1
+    return i,process_time()-start,acc.to_bytes(KEY_BYTES,'little')
--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -1,16 +1,42 @@
 from sqrl import KEY_BYTES
 from sqrl.crypto import *
 
+import os
+import csv
 from base64 import urlsafe_b64decode
+from binascii import a2b_hex, b2a_hex
+
+EXPENSIVE_TESTS = os.environ.get("EXPENSIVE_TESTS")
+
 
 def test_enhash():
     with open('tests/enhash-vectors.txt') as fi:
         for line in fi:
-            a = line[:43]+'='
-            b = line[43:86]+'='
+            a = line[:43] + '='
+            b = line[43:86] + '='
             key = urlsafe_b64decode(a)
             expected = urlsafe_b64decode(b)
-            assert enhash(key)==expected
+            assert enhash(key) == expected
+
+
+def test_enscrypt():
+    '''this is a very time-consuming test (by design).'''
+
+    with open('tests/enscrypt-vectors.csv') as fi:
+        for row in csv.reader(fi):
+            print(row)
+            if not row:
+                continue
+            password, salt, logN, iterations, key = row
+            i = int(iterations)
+            if EXPENSIVE_TESTS or (i < 20):
+                i, t, dkey = enscrypt(
+                    password.encode('utf-8'),
+                    a2b_hex(salt),
+                    int(logN), i)
+                print(row,i,t)
+                assert b2a_hex(dkey).decode() == key
 
 if __name__ == '__main__':
     test_enhash()
+    test_enscrypt()
